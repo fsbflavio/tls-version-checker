9cc5673ddab09b065afc98d9a614f22ae7caa8c6
flavio <flavio.baia@pronetsoftware.com.br>
2024-04-07 21:18:47 -0300

    Allow use a client certificate for authentication

--- TlsVersionChecker/Program.cs
+++ TlsVersionChecker/Program.cs
@@ -1,91 +1,121 @@
 ﻿using System;
+using CommandLine;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Net.Security;
 using System.Net.Sockets;
 using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.ConstrainedExecution;
+using CommandLine.Text;
 
 class Program
 {
-    static void Main(string[] args)
+    //[("tcp", isDefault: true, HelpText = "Connect using TcpClient")]
+    class UseTcpClient
     {
-        if (args.Length < 1 || Array.Exists(args, arg => arg.Equals("--help", StringComparison.OrdinalIgnoreCase)))
-        {
-            ShowHelp();
-            return;
-        }
-        // Use the first command-line argument as the target host
-        string targetHost = args[0];
+        [Option('t', "target-host", Required = true, HelpText = "The host to connect with")]
+        public string TargetHost { get; set; }
 
-        // Check for the optional "--use" parameter
-        SslProtocols desiredTlsVersion = SslProtocols.None;
-        for (int i = 1; i < args.Length - 1; i++)
-        {
-            if (args[i].Equals("--use", StringComparison.OrdinalIgnoreCase))
-            {
-                string tlsVersion = args[i + 1].ToLowerInvariant();
-                Console.WriteLine(tlsVersion);
-                switch (tlsVersion)
-                {
-                    case "tls13":
-                        desiredTlsVersion = SslProtocols.Tls13;
-                        break;
-                    case "tls12":
-                        desiredTlsVersion = SslProtocols.Tls12;
-                        break;
-                    case "tls11":
-                        desiredTlsVersion = SslProtocols.Tls11;
-                        break;
-                    case "tls":
-                        desiredTlsVersion = SslProtocols.Tls;
-                        break;
-                    default:
-                        Console.WriteLine("Invalid TLS version. Use one of the following: tls13, tls12, tls11, tls.");
-                        return;
-                }
-            }
-            else
-            {
-                Console.WriteLine($"Invalid parameter: {args[i]}");
-                ShowHelp();
-                return;
-            }
-        }
+        [Option('u', "use-tls-version", Default = "system", Required = false, HelpText = "Force the use of a specific TLS version (e.g., tls13, tls12, tls11, tls, system)")]
+        public string TlsVersion { get; set; }
 
-        // Create a TCP connection to the host
-        using TcpClient tcpClient = new TcpClient(targetHost, 443);
+        [Option('c', "cert-path", Required = false, HelpText = "Path to client certificate to be used in connection")]
+        public string CertificatePath { get; set; }
 
-        // Establish an SSL/TLS connection over the TCP connection
-        using SslStream sslStream = new SslStream(tcpClient.GetStream(), false);
+        [Option('p', "cert-pwd", Required = false, HelpText = "Certificate password")]
+        public string CertificatePassword { get; set; }
 
-        try
+        [Usage(ApplicationAlias = "TlsVersionChecker.exe")]
+        public static IEnumerable<Example> Examples
         {
-            if (desiredTlsVersion != SslProtocols.None)
+            get
             {
-                // Authenticate the SSL/TLS connection with the target host using the specified TLS version
-                sslStream.AuthenticateAsClient(targetHost, null, desiredTlsVersion, false);
+                return new List<Example>() {
+                    new Example("Conect to host using default system TLS config", new UseTcpClient { TargetHost = "example.com" }),
+                    new Example("Conect to host using TLS 1.2", new UseTcpClient { TargetHost = "example.com", TlsVersion = "tls12" }),
+                    new Example("Conect to host using a client certificate", new UseTcpClient { TargetHost = "example.com", CertificatePath = "C:/cert.pfx", CertificatePassword = "123456" })
+                };
             }
-            else
-            {
-                // Authenticate the SSL/TLS connection with the target host without specifying a TLS version
-                sslStream.AuthenticateAsClient(targetHost);
-            }
-            Console.WriteLine("Values being used:");
-            Console.WriteLine($"Negotiated Cipher Suite: {sslStream.NegotiatedCipherSuite}");
-            Console.WriteLine($"Cipher: {sslStream.CipherAlgorithm}");
-            Console.WriteLine($"Hash Algorithm: {sslStream.HashAlgorithm}");
-            Console.WriteLine($"Key Exchange Algorithm: {sslStream.KeyExchangeAlgorithm}");
-            Console.WriteLine($"SSL/TLS Protocol version: {sslStream.SslProtocol}");
-        }
-        catch (AuthenticationException ex)
-        {
-            Console.WriteLine($"Error establishing SSL/TLS connection: {ex}");
         }
     }
 
-    private static void ShowHelp()
+    private static int ConectUsingTcpClient(UseTcpClient opts)
     {
-        Console.WriteLine("Usage: TlsVersionChecker.exe <target_host> [--use tls13|tls12|tls11|tls]");
-        Console.WriteLine("\nOptions:");
-        Console.WriteLine("  --help          Show this help message");
-        Console.WriteLine("  --use VERSION   Force the use of a specific TLS version (e.g., tls13, tls12, tls11, tls)");
+        try
+        {
+            SslProtocols desiredTlsVersion = SslProtocols.None;
+
+            switch (opts.TlsVersion)
+            {
+                case "tls13":
+                    desiredTlsVersion = SslProtocols.Tls13;
+                    break;
+                case "tls12":
+                    desiredTlsVersion = SslProtocols.Tls12;
+                    break;
+                case "tls11":
+                    desiredTlsVersion = SslProtocols.Tls11;
+                    break;
+                case "tls":
+                    desiredTlsVersion = SslProtocols.Tls;
+                    break;
+                case "system":
+                    desiredTlsVersion = SslProtocols.None;
+                    break;
+                default:
+                    Console.WriteLine("Invalid TLS version. Use one of the following: tls13, tls12, tls11, tls, system");
+                    return 0;
+            }
+
+            using (TcpClient tcpClient = new TcpClient(opts.TargetHost, 443))
+            using (SslStream sslStream = new SslStream(tcpClient.GetStream(), false))
+            {
+                X509CertificateCollection certCollection = null;
+                if (!String.IsNullOrWhiteSpace(opts.CertificatePath) && !String.IsNullOrWhiteSpace(opts.CertificatePassword))
+                {
+                    X509Certificate2 certificate = new X509Certificate2(opts.CertificatePath, opts.CertificatePassword);
+                    certCollection = new X509CertificateCollection { certificate };
+                }
+
+                if (desiredTlsVersion != SslProtocols.None)
+                {
+                    sslStream.AuthenticateAsClient(opts.TargetHost, certCollection, desiredTlsVersion, false);
+                }
+                else
+                {
+                    sslStream.AuthenticateAsClient(opts.TargetHost, certCollection, false);
+                }
+                Console.WriteLine("Values being used:");
+#if NET5_0_OR_GREATER
+                Console.WriteLine($"Negotiated Cipher Suite: {sslStream.NegotiatedCipherSuite}");
+#endif
+                Console.WriteLine($"Cipher: {sslStream.CipherAlgorithm}");
+                Console.WriteLine($"Cipher strength: {sslStream.CipherStrength}");
+                Console.WriteLine($"Hash Algorithm: {sslStream.HashAlgorithm}");
+                Console.WriteLine($"Key Exchange Algorithm: {sslStream.KeyExchangeAlgorithm}");
+                Console.WriteLine($"SSL/TLS Protocol version: {sslStream.SslProtocol}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error establishing SSL/TLS connection using TcpClient:\r\n {ex}");
+        }
+        return 0;
+    }
+
+    static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<UseTcpClient>(args)
+            .MapResult(
+              (UseTcpClient opts) => ConectUsingTcpClient(opts),
+              errs => 1);
     }
 }
